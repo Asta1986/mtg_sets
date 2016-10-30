710c2aed6af3770e8c5e05df120c3620425b68d5
Asta666 <ltee885@gmail.com>
2016-10-30 15:05:13 -0300

    update

---
+++ rename_sets_test.py
@@ -0,0 +1,23 @@
+from rename_sets import rename_files
+import os
+import unittest
+import shutil
+
+class TestRenameSetsScript(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        os.makedirs('test_dir')
+
+    def test_file_rename(self):
+        test_file_name = 'AetherAerathi[#].xlhq.img'
+        expected_file_name = 'AEtherAErathi#.full.img'
+        test_dir = 'test_dir'
+        os.system('touch ' + test_dir + '/' + test_file_name)
+        rename_files(test_dir)
+        self.assertTrue(os.path.exists(test_dir + '/' + expected_file_name))
+        self.assertFalse(os.path.exists(test_dir + '/' + test_file_name))
+
+    @classmethod
+    def tearDownClass(self):
+        shutil.rmtree('test_dir/')
